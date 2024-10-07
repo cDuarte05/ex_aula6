9db46c96c2bc89046fa0a14764e4ebd9cb5d2161
Henrique <hcduarte200@gmail.com>
2024-10-07 14:49:15 -0300

    Menu feito e outros

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Bem vindo</h1>
+    <p>Selecione a opção desejada: </p>
+    <div class="menu">
+        <p><input type="button" onclick ="window.location.href='gravar.html'" value="Gravar dados" ></p>
+        <p><input type="button" onclick = "window.location.href='dados.txt'" value="Ver dados"></p>
+    </div>
+
+    <!-- CONTADOR DE VISITAS -->
+<?php
+    $arqVisitantes = fopen("contVisita.txt","r+");
+    $lerVisitas = fread($arqVisitantes,10);
+    $contador= (int)$lerVisitas+1;
+    echo "Você é o visitante número: $contador";
+    fseek($arqVisitantes, 0); //fseek irá colocar o ponteiro no início do arquivo texto aberto
+    fwrite($arqVisitantes, $contador); //Grava no arquivo texto o valor da variável contador
+?>
+
+</body>
+</html>
